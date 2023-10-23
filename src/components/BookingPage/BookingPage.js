621d175a52436c238193961932abe720b7b9f583
KodeReact <kaniketejaswini3@gmail.com>
2023-10-23 18:04:58 +0530

    second commit

--- src/components/BookingPage/BookingPage.js
+++ src/components/BookingPage/BookingPage.js
@@ -9,12 +9,13 @@ import WeekendSharpIcon from '@mui/icons-material/WeekendSharp';
 function BookingPage() {
   return (
     <div className='booking-container'>
+        {/* Ticket type, Qty */}
         <Selection />
-        {/* seats */}
+        {/* all seats display*/}
         <div className='seat-map'>
            <Seat />
         </div>
-  
+        {/* available, unavailable, your selected seats  */}
         <div className='referance-container'>
            <h3>Key to Seat</h3>
           <div className='icons-container'>
@@ -29,7 +30,7 @@ function BookingPage() {
             <WeekendSharpIcon style={{ height: '40px', width: '40px', fill:'#50C878' }}  />
             <span className='span'>Your Selection</span>
           </div>
-        
+          {/* proceed button */}
           <button className='proceed-btn'>Proceed</button>
        </div>
         
